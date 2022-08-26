a736a4cbf02f632a5c23b7a4796edca2d32eee62
Chris Scott <chris@chrisdjscott.co.uk>
2022-08-26 16:07:51 +1200

    adding tensorflow to python 3.10 kernel
    also removing unset of I_MPI_PMI_LIBRARY as no longer being set by the
    module

--- bin/python3105-gimkl-2022a.sh
+++ bin/python3105-gimkl-2022a.sh
@@ -4,6 +4,6 @@ echo "Loading NeSI kernel: Python/3.10.5-gimkl-2022a"
 
 module purge
 module load Python/3.10.5-gimkl-2022a
-unset I_MPI_PMI_LIBRARY
+module load TensorFlow/2.8.2-gimkl-2022a-Python-3.10.5
 
 exec python -m ipykernel_launcher $@
